1c3ef347aa52265dfdd1be0b90df104ef0c5a850
Anatol Hancharou <a.potteroff@gmail.com>
2018-02-20 19:29:33 +0300

    love-triangle solution

--- src/index.js
+++ src/index.js
@@ -4,4 +4,25 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  preferences.unshift(0);
+  var count = 0;
+  var i;
+  
+  for (i = 1; i < preferences.length; i++) {
+    if (preferences[i] === i) {
+      preferences[i] = 0;
+    }
+  }
+  
+  for (i = 1; i < preferences.length; i++) {
+    if (preferences[i] === 0) continue;
+      else if (preferences[preferences[preferences[i]]] === i) {
+        count++;
+        preferences[preferences[preferences[i]]] = 0;
+        preferences[preferences[i]] = 0;
+        preferences[i] = 0;
+      }
+  }
+    
+  return count;
 };
